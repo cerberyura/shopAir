95affaa683a7e9ff996383a213c14d8a4898cf16
Yurii Cerber <yuriicerber@gmail.com>
2023-09-11 23:54:09 +0000

    first history

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    app.listen(port, () => {
+      console.log('App listen port ' + port)
+    })
+  })
