d36da7bd950ea30f8238375d1d33a840ba4b5d68
Yurii Cerber <yuriicerber@gmail.com>
2023-09-16 21:54:02 +0000

    ReactRouterPath

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {HomePage} from "./pages/HomPage";
+import {Path} from "./Path";
 
 const App = () => {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={Path.home} element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
